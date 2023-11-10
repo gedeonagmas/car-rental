ef1c43e187a63ff453765b894041af7ccb76f236
gedeonagmas <103260131+gedeonagmas@users.noreply.github.com>
2023-11-10 10:54:06 +0300

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 async function mongodb() {
-  await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }); //replace by MONGO_URL
+  await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }); //replace by MONGO_URL
   console.log("car database connected successfully");
 }
 
